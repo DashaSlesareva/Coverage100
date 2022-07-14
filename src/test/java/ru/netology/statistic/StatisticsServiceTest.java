597c2964cf5842a50fa6eeede4870beb955e4b29
DashaSlesareva <zalezka@yandex.ru>
2022-07-14 12:16:13 +0300

    Test Coveragr 100

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,15 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StatisticsServiceTest {
 
-    @Test
-    void findMax() {
-        StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    @Test
+    void findMaxFirstMassive() {
+        StatisticsService service = new StatisticsService();
         long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
 
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxSecondMassive() {
+        StatisticsService service = new StatisticsService();
+        long expected = 12;
+        long[] incomesInBillions = {5, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 }
